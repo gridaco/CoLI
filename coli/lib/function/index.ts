d554695c8f241db123c086ca643a7362cb2a1634
GwonHeeJun <developer_gwon@naver.com>
2021-02-28 03:41:32 +0900

    modify : Function class & Examples

--- coli/lib/function/index.ts
+++ coli/lib/function/index.ts
@@ -1,3 +1,43 @@
-export class Function {
+import { Type, Types } from "../type";
 
+type Params = { argName: string, argType: Type }[]
+
+export class Function {
+    functionName: string;
+    functionArguments: Params;
+    functionReturnType: Type;
+
+    constructor(funcName: string, args?: Params, returnType?: Type) {
+        this.functionName = funcName;
+        this.functionArguments = args;
+        this.functionReturnType = returnType;
+    }
+
+    public args(...args: Params) {
+        this.functionArguments = args
+        return this;
+    }
+
+    public returnType(type: Type) {
+        this.functionReturnType = type
+        return this;
+    }
+
+    public call(...value: any) {
+        return this;
+    }
 }
+
+/** Example */
+
+new Function("test").args({ argName: "testArg", argType: Types.any }).returnType(Types.any)
+// -> function test( testArg : any ) : any
+
+new Function("test", [{ argName: "testArg", argType: Types.any }]).returnType(Types.any)
+// -> function test( testArg : any ) : any
+
+new Function("test", [{ argName: "testArg", argType: Types.any }], Types.any)
+// -> function test( testArg : any ) : any
+
+new Function("test", [{ argName: "testArg", argType: Types.any }], Types.any).call()
+// -> test()
