46f6a436c3a4cb3817f3091280fd40a4727d248b
GwonHeeJun <developer_gwon@naver.com>
2021-02-28 03:37:37 +0900

    modify : create Type & Types. TS

--- coli/lib/type/index.ts
+++ coli/lib/type/index.ts
@@ -1,3 +1,17 @@
-export interface Type {
-
+export class Type {
+    constructor(readonly name: string) { }
 }
+
+export class Types {
+    static any: Type = new Type("any");
+    static string: Type = new Type("string");
+    static number: Type = new Type("number");
+    static bigint: Type = new Type("bigint");
+    static object: Type = new Type("object");
+    static boolean: Type = new Type("boolean");
+    static unknown: Type = new Type("unknown");
+    static void: Type = new Type("void");
+    static undefined: Type = new Type("undefined");
+    static never: Type = new Type("never");
+}
+
