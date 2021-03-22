ba9e39d26c2dac865ab6ec86c878cfcb77afe348
GwonHeeJun <developer_gwon@naver.com>
2021-03-23 00:01:56 +0900

    WIP

--- coli/lib/function/index.ts
+++ coli/lib/function/index.ts
@@ -1,19 +1,19 @@
 import { Type, Types } from "../type";
 
-type Params = { argName: string; argType: Type }[];
-
 export class Function {
-  functionName: string;
-  functionArguments: Params;
-  functionReturnType: Type;
+  private functionName: string;
+  private functionArguments: object;
+  private functionReturnType: Type;
+  private code: string;
+  innerContent: string;
 
-  constructor(funcName: string, args?: Params, returnType?: Type) {
+  constructor(funcName: string, args?: object, returnType?: Type) {
     this.functionName = funcName;
     this.functionArguments = args;
     this.functionReturnType = returnType;
   }
 
-  public args(...args: Params) {
+  public args(args: object) {
     this.functionArguments = args;
     return this;
   }
@@ -23,7 +23,34 @@ export class Function {
     return this;
   }
 
-  public call(...value: any) {
+  public content(content: any) {
+    this.innerContent = content;
     return this;
+  }
+
+  public call() {
+    this.code = `function ${this.functionName}`;
+
+    if (this.functionArguments != null) {
+      const paramLength = Object.keys(this.functionArguments).length;
+      let params = "(";
+      Object.keys(this.functionArguments).map((i, ix) => {
+        if (paramLength == ix + 1) {
+          params += ` ${i} : ${this.functionArguments[i].type}`;
+        } else {
+          params += `${i} : ${this.functionArguments[i].type},`;
+        }
+      });
+      this.code += `${params}) : `;
+    }
+
+    this.code += `${
+      this.functionReturnType == null ? "any" : this.functionReturnType.type
+    } {`;
+
+    this.code += this.innerContent;
+
+    this.code += `}`;
+    return this.code;
   }
 }
