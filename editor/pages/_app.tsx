15edddb378df1c38e5985d2ba6e0babeca2ad92a
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-23 20:15:41 +0900

    add seo meta properties

--- editor/pages/_app.tsx
+++ editor/pages/_app.tsx
@@ -10,11 +10,12 @@ function MyApp({ Component, pageProps }) {
         styles={css`
           body {
             margin: 0px;
-            font-family: 'Roboto', sans-serif;
+            font-family: "Roboto", sans-serif;
           }
         `}
       />
       <Head>
+        <SeoMeta />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&"
           rel="stylesheet"
@@ -26,6 +27,18 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </div>
     </React.Fragment>
+  );
+}
+
+function SeoMeta() {
+  return (
+    <>
+      <meta property="title" content="CoLI Designer" />
+      <meta
+        property="description"
+        content="Code builder for coders. replace your developers with CoLI"
+      />
+    </>
   );
 }
 
