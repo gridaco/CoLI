2f47ca1360387dad0b9cb83106a7a49ef8b674ea
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-25 18:05:07 +0900

    add ImportDefaultSpecifier's contructor

--- packages/coli/lib/declarations/import/import-default.specifier.ts
+++ packages/coli/lib/declarations/import/import-default.specifier.ts
@@ -5,7 +5,16 @@ export class ImportDefaultSpecifier extends BaseImportSpecifier {
     name: string;
   };
 
-  constructor() {
+  /**
+   * ImportDefault does only requires the name of the import, which is named "local"
+   * @param params
+   */
+  constructor(params: { local: string }) {
     super("ImportDefaultSpecifier");
+
+    // set local.name with local
+    this.local = {
+      name: params.local,
+    };
   }
 }
