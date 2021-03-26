cbf8f5e6fc32c53dc572ca400f9305858bb000eb
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-26 13:24:09 +0900

    wip - function declaration

---
+++ packages/coli/lib/builders/block-statement/empty-block.ts
@@ -0,0 +1,4 @@
+import { ColiBuilder } from "../../builder/builder";
+import { BlockStatement } from "../../statements";
+
+export class EmptyBlockStatement extends BlockStatement {}
--- packages/coli/lib/declarations/function/index.ts
+++ packages/coli/lib/declarations/function/index.ts
@@ -1,15 +1,32 @@
 import { Identifier } from "../../ast/identifier";
-import { Type } from "../../builders/type";
+import { Type, Types } from "../../builders/type";
 import { BlockStatement } from "../../statements";
 import { _DECLARATION_FUNCTION } from "../../_internal/constants/declarations-name";
 import { Declaration } from "../declaration.base";
-
+import assert from "assert";
+import { EmptyBlockStatement } from "../../builders/block-statement/empty-block";
 export class FunctionDeclraration extends Declaration {
   id: Identifier;
-  params: [];
-  body: BlockStatement;
-  returnType: Type;
-  constructor() {
+  params: [] = [];
+  body: BlockStatement = new EmptyBlockStatement();
+  returnType: Type = Types.any;
+
+  constructor(
+    name: string,
+    args: {
+      returnType?: Type;
+      body?: BlockStatement;
+    }
+  ) {
     super(_DECLARATION_FUNCTION);
+    // validate function name
+    assert(/^[$A-Z_][0-9A-Z_$]*$/i.test(name), "invalid function name");
+
+    // set return type. default value is provided.
+    if (args.returnType) {
+      this.returnType = args.returnType;
+    }
   }
 }
+
+const test = new FunctionDeclraration("test", {});
