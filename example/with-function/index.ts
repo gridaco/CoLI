7d759de197fcd8781c8bf9fe773da1a232fdea51
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-26 15:23:34 +0900

    update function example

--- example/with-function/index.ts
+++ example/with-function/index.ts
@@ -1,14 +1,17 @@
-import { Function } from "coli/lib";
+import { Function, Block, Snippet } from "coli/lib";
 import { Type, Types } from "coli/lib";
 
 /** Example */
-new Function("test")
+new Function("sum")
   .withParams(
-    { name: "a", typeAnnotation: Types.boolean },
-    { name: "b", typeAnnotation: Types.boolean }
+    { name: "a", typeAnnotation: Types.number },
+    { name: "b", typeAnnotation: Types.number }
   )
-  .returns(Types.any);
-// -> function test( testArg : any ) : any
+  .withBody(new Block().add(Snippet.fromStatic("return a + b")))
+  .returns(Types.number);
+/// function sum( a: number, b: number ) : number {
+///   return a + b
+/// }
 
 new Function("test", [{ name: "b", typeAnnotation: Types.boolean }]).returns(
   Types.any
