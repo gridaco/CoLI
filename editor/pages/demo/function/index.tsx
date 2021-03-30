f355031ce67ecc70796b2786909cecd5f539601e
GwonHeeJun <developer_gwon@naver.com>
2021-03-31 04:48:18 +0900

    wip - function demo

--- editor/pages/demo/function/index.tsx
+++ editor/pages/demo/function/index.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import { Types } from "coli/lib";
+import React from "react";
+import FunctionDeclaration, {
+  FunctionDeclaration as FunctionDeclarationInterface,
+} from "../../../components/declarations/function";
+import styled from "@emotion/styled";
+import CodeBlock from "../../../components/code-block";
+import { Identifier } from "coli/lib/ast/identifier";
+
+const functionDefaultData: {
+  example: string;
+  declarations: FunctionDeclarationInterface[];
+} = {
+  example: `let variableName : any = 0`,
+  declarations: [
+    {
+      name: "sum",
+      args: {
+        params: [
+          new Identifier("a", {
+            typeAnnotation: Types.number,
+          }),
+          new Identifier("b", {
+            typeAnnotation: Types.number,
+          }),
+        ],
+      },
+    },
+  ],
+};
 
 function CoLiFunctionDemoPage() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Wrapper>
+      <div className="declarations-view">
+        <CodeBlock>{functionDefaultData.example}</CodeBlock>
+        {functionDefaultData.declarations.map((i, ix) => (
+          <FunctionDeclaration id={ix} data={i} key={ix} />
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
-export default CoLiFunctionDemoPage
+export default CoLiFunctionDemoPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  .declarations-view {
+    width: calc(100% - 175px);
+    margin-top: 35px;
+    margin-left: 175px;
+    flex: 1;
+
+    pre {
+      font-size: 12px !important;
+      padding-left: 0px !important;
+    }
+  }
+`;
