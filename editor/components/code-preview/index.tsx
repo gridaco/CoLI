e6dc3a389173f64a40ab0b1eb0a8d307e1b413e4
GwonHeeJun <developer_gwon@naver.com>
2021-03-30 20:51:37 +0900

    change comment preview struct

--- editor/components/code-preview/index.tsx
+++ editor/components/code-preview/index.tsx
@@ -5,71 +5,78 @@ import { useRecoilValue } from "recoil";
 import { stringfy, StringfyLanguage } from "../../../packages/export-string";
 import { currentColiEditorOption } from "../../states/option.state";
 
-const valueToInterfaceData = (data: object) => {
-  let code = [];
+// const valueToInterfaceData = (data: object) => {
+//   let code = [];
 
-  Object.keys(data).map((i) => {
-    switch (typeof data[i]) {
-      case "string":
-        code.push(`${i} : "${data[i]}"`);
-        break;
-      case "object":
-        let objectCodes = {};
-        objectCodes[i] = [];
-        if (data[i] instanceof Array) {
-          data[i].map((_i) => {
-            let tempArray = [];
-            const { type, ...result } = _i;
+//   Object.keys(data).map((i) => {
+//     switch (typeof data[i]) {
+//       case "string":
+//         code.push(`${i} : "${data[i]}"`);
+//         break;
+//       case "object":
+//         let objectCodes = {};
+//         objectCodes[i] = [];
+//         if (data[i] instanceof Array) {
+//           data[i].map((_i) => {
+//             let tempArray = [];
+//             const { type, ...result } = _i;
 
-            Object.keys(result).map((key) => {
-              tempArray.push(`${key} : ${JSON.stringify(_i[key])}`);
-            });
+//             Object.keys(result).map((key) => {
+//               tempArray.push(`${key} : ${JSON.stringify(_i[key])}`);
+//             });
 
-            objectCodes[i].push(
-              `  new ${_i.type}({\n\t\t${tempArray.join(",\n\t\t")}\n\t  })`
-            );
-          });
-          code.push(`${i} : [\n\t${Object.values(objectCodes[i]).join(",\n\t")}\n\t]`);
-        }
-        break;
-      default:
-        break;
-    }
-  });
+//             objectCodes[i].push(
+//               `  new ${_i.type}({\n\t\t${tempArray.join(",\n\t\t")}\n\t  })`
+//             );
+//           });
+//           code.push(
+//             `${i} : [\n\t${Object.values(objectCodes[i]).join(",\n\t")}\n\t]`
+//           );
+//         }
+//         break;
+//       default:
+//         break;
+//     }
+//   });
 
-  return code.join(",\n  ");
-};
+//   return code.join(",\n  ");
+// };
 
-const codePreviewCommentInterface = (params: {
-  interface: any;
+// const codePreviewCommentInterface = (params: {
+//   interface: any;
+//   value: object;
+//   lauangue: StringfyLanguage;
+// }) => {
+//   let comments = "";
+//   if (params.lauangue === "typescript") {
+//     comments = `/***
+// new ${params.interface.name}({
+//   ${valueToInterfaceData(params.value)}
+// })
+// */`;
+//   } else if (params.lauangue === "python") {
+//     comments = `'''
+// new ${params.interface.name}({
+//   ${valueToInterfaceData(params.value)}
+// })
+// '''`;
+//   } else if (params.lauangue === "dart") {
+//     comments = `/**
+// new ${params.interface.name}({
+//   ${valueToInterfaceData(params.value)}
+// })
+// */`;
+//   }
+
+//   return comments;
+// };
+
+export function CodePreview(props: {
   value: object;
-  lauangue: StringfyLanguage;
-}) => {
-  let comments = "";
-  if (params.lauangue === "typescript") {
-    comments = `/***
-new ${params.interface.name}({
-  ${valueToInterfaceData(params.value)}
-})
-*/`;
-  } else if (params.lauangue === "python") {
-    comments = `'''
-new ${params.interface.name}({
-  ${valueToInterfaceData(params.value)}
-})
-'''`;
-  } else if (params.lauangue === "dart") {
-    comments = `/**
-new ${params.interface.name}({
-  ${valueToInterfaceData(params.value)}
-})
-*/`;
-  }
-
-  return comments;
-};
-
-export function CodePreview(props: { value: object; interface: any }) {
+  interface: any;
+  // will be required
+  codeHandler?: (args: { class: any; value: object }) => void;
+}) {
   const editorOption = useRecoilValue(currentColiEditorOption);
 
   return (
@@ -79,12 +86,13 @@ ${stringfy(new props.interface(props.value), {
   language: editorOption.lauangue as StringfyLanguage,
 })}
 
-${codePreviewCommentInterface({
+${props.codeHandler({ class: props.interface, value: props.value })}
+    `}
+      {/* ${codePreviewCommentInterface({
   value: props.value,
   interface: props.interface,
   lauangue: editorOption.lauangue as StringfyLanguage,
-})}
-    `}
+})} */}
     </Wrapper>
   );
 }
