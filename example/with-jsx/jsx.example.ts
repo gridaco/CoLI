8481637fe15248e4798abac2d224f81defe8f810
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-01 03:31:55 +0900

    add jsx example

--- example/with-jsx/jsx.example.ts
+++ example/with-jsx/jsx.example.ts
@@ -1,2 +1,45 @@
+import { stringfy } from "@coli/export-string";
+import { JSXElement, JSXIdentifier } from "coli/lib/jsx";
+import { JSXClosingElement } from "coli/lib/jsx/jsx-closing-element";
 import { JSXExpression } from "coli/lib/jsx/jsx-expression";
+import { JSXOpeningElement } from "coli/lib/jsx/jsx-opening-element";
 const JSX = new JSXExpression();
+
+const wrapperJsxIdentifier = new JSXIdentifier("Wrapper");
+const titleAndAvatarWrapperJsxIdentifier = new JSXIdentifier(
+  "TitleAndAvatarWrapper"
+);
+const titleJsxIdentifier = new JSXIdentifier("Title");
+
+/**
+ * <Wrapper>
+ *      <TitleAndAvatarWrapper>
+ *          <Title>{props.title}</Title>
+ *          <Avatar src={props.avatar}/>
+ *      </TitleAndAvatarWrapper>
+ *      <Message>{props.message}</Message>
+ *  </Wrapper>
+ */
+
+const jsx = new JSXElement({
+  openingElement: new JSXOpeningElement(wrapperJsxIdentifier),
+  closingElement: new JSXClosingElement(wrapperJsxIdentifier),
+  children: [
+    new JSXElement({
+      openingElement: new JSXOpeningElement(titleAndAvatarWrapperJsxIdentifier),
+      closingElement: new JSXClosingElement(titleAndAvatarWrapperJsxIdentifier),
+      children: [
+        new JSXElement({
+          openingElement: new JSXOpeningElement(titleJsxIdentifier),
+          closingElement: new JSXClosingElement(titleJsxIdentifier),
+        }),
+      ],
+    }),
+  ],
+});
+
+console.log(
+  stringfy(jsx, {
+    language: "tsx",
+  })
+);
