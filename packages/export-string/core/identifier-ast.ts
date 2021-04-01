f4456dac525567281690edb59f411d967a573dac
GwonHeeJun <developer_gwon@naver.com>
2021-04-01 20:58:40 +0900

    add json type annotation

--- packages/export-string/core/identifier-ast.ts
+++ packages/export-string/core/identifier-ast.ts
@@ -1,6 +1,16 @@
 import { Identifier } from "coli/lib/ast";
 import { StringfyLanguage } from "..";
 
+/*@internal*/
+function isJson(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
 export function coliIdentifierStringfy(
   c: Identifier,
   l: StringfyLanguage
@@ -9,7 +19,12 @@ export function coliIdentifierStringfy(
   let code = `${name}`;
 
   if (typeAnnotation) {
-    code += ` : ${typeAnnotation.keyword}`;
+    if (isJson(typeAnnotation.keyword)) {
+      // If type is defined as object, delete "
+      code += ` : ${typeAnnotation.keyword.replace(/"/gi, "")}`;
+    } else {
+      code += ` : ${typeAnnotation.keyword}`;
+    }
   }
 
   return code;
