1d4d3425226db2628bc6935c307fca70d7574e78
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-01 16:08:09 +0900

    fix Block constructor

--- packages/coli/lib/builders/block/block.ts
+++ packages/coli/lib/builders/block/block.ts
@@ -2,12 +2,11 @@ import { BlockStatement } from "../../statements";
 import { ColiObject } from "../../_abstract";
 
 export class Block extends BlockStatement {
-  constructor(body: ColiObject) {
-    super(body);
+  constructor(...body: ColiObject[]) {
+    super(...body);
   }
 
   add(body: ColiObject): this {
-    throw "this is tempporarily dead";
     this.body.push(body);
     return this;
   }
