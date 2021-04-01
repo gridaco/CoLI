4d891db8d3d73973cf340fc28b3beca65e27af56
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-01 22:18:44 +0900

    qf

--- packages/export-string/core/jsx-text.ts
+++ packages/export-string/core/jsx-text.ts
@@ -2,5 +2,5 @@ import { JSXText } from "coli/lib/jsx/jsx-text";
 import { StringfyLanguage } from "..";
 
 export function coliJSXTextStringfy(c: JSXText, l: StringfyLanguage): string {
-  return "c.value";
+  return c.value;
 }
