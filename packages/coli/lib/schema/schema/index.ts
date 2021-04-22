14db8be73f693894cfd199a4c48d09de38266c25
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-23 02:42:10 +0900

    add export str_

--- packages/coli/lib/schema/schema/index.ts
+++ packages/coli/lib/schema/schema/index.ts
@@ -1,4 +1,4 @@
 export { Keywords } from "./keywords";
 export { IProperty, Property, property } from "./property";
 export { SchemaBuilder } from "./schema";
-export { str_kvline, str_kvlines, str_property } from "./str_";
+export { str_kvline, str_kvlines, str_property, format } from "./str_";
