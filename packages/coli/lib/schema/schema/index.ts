0ded8b9643febae0373f3e35a4019e9cb242b562
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-23 03:21:52 +0900

    add editor build command & fix build error

--- packages/coli/lib/schema/schema/index.ts
+++ packages/coli/lib/schema/schema/index.ts
@@ -1,4 +1,5 @@
 export { Keywords } from "./keywords";
-export { IProperty, Property, property } from "./property";
+export { Property, property } from "./property";
+export type { IProperty } from "./property";
 export { SchemaBuilder } from "./schema";
 export { str_kvline, str_kvlines, str_property, format } from "./str_";
