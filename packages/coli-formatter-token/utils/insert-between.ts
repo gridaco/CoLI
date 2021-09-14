25116ba0b8b0a02ee76b0226c94cb3b1fc73c1ed
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-14 14:21:04 +0900

    fix insert-between

--- packages/coli-formatter-token/utils/insert-between.ts
+++ packages/coli-formatter-token/utils/insert-between.ts
@@ -1,5 +1,5 @@
 export function insertBetween(arr: any[], insert: any) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i <= arr.length; i++) {
     if (i !== 0 && i !== arr.length - 1) {
       arr.splice(i, 0, insert);
     }
