e2035df31afe79fbaa7de88dc3fdf118b8f71178
softmarshmallow <woojoo@softmarshmallow.com>
2022-02-19 12:38:03 +0900

    fix coli type

--- packages/coli-core/expressions/call-expression/index.ts
+++ packages/coli-core/expressions/call-expression/index.ts
@@ -1,11 +1,11 @@
-import type { ColiObject } from "../..";
+import type { ColiObject, Identifier } from "../..";
 import { _EXPRESSION_CALL } from "../../_internal/node-name/expressions-name";
 import { Expression } from "../expression.base";
 
 export class CallExpression extends Expression {
   readonly arguments?: Array<ColiObject>;
   constructor(
-    readonly expression: Expression,
+    readonly expression: Expression | Identifier,
     _arguments?: ColiObject | Array<ColiObject>
   ) {
     super(_EXPRESSION_CALL);
