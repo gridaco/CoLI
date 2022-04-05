ce1fd85cbc177ce4c3b48c0f584e24d2e7441cd2
UZU, J <woojoo@softmarshmallow.com>
2022-04-05 18:03:50 +0900

    update jsx atrribute builder to allow null initializer

--- packages/coli-formatter-token/formatters/jsx/jsx-atrribute.ts
+++ packages/coli-formatter-token/formatters/jsx/jsx-atrribute.ts
@@ -4,6 +4,12 @@ import f from "../../tokens";
 
 export function astfmt_jsx_attribute(atrribute: JSXAttribute) {
   const { name, initializer } = atrribute;
+  // e.g. <input readonly />
+  if (!initializer) {
+    return [name];
+  }
+
   // key=val
+  // e.g. <input key="value" />
   return [name, f(SyntaxKind.EqualsToken), initializer];
 }
